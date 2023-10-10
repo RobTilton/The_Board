28ff9acb27a20c7f394f141241b3c9d413ccab69
Robert T <RobertJTilton@gmail.com>
2023-10-10 14:52:17 -0400

    added numbers to boards, setup for multi boards

--- webpages/models.py
+++ webpages/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Board(models.Model):
     name = models.CharField(max_length=100)
+    board_number = models.IntegerField(max_length=10, default = 0, unique=True)
 
     def __str__(self):
         return self.name
